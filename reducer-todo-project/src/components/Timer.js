cf4d49a0e7cd76e99862eec0738e3b555cd745e9
masonkarsevar <masonkarsevar@gmail.com>
2019-08-13 12:44:40 -0700

    basic timer framework finished. WIP: how to send state from Form to timer

--- reducer-todo-project/src/components/Timer.js
+++ reducer-todo-project/src/components/Timer.js
@@ -1,9 +1,41 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Timer(props) {
+    const [seconds, setSeconds] = useState(0);
+    const [isActive, setIsActive] = useState(false);
+
+    function toggle(event) {
+        event.stopPropagation();
+        setIsActive(!isActive);
+    }
+
+    function reset() {
+        setSeconds(0);
+        setIsActive(false);
+    }
+    useEffect(() => {
+        let interval = null;
+        if(isActive) {
+            interval = setInterval(() => {
+                setSeconds(seconds => seconds + 1);
+            }, 1000);
+        } else if (!isActive && seconds !== 0) {
+            clearInterval(interval);
+        }
+
+        return () => clearInterval(interval);
+    }, [isActive, seconds]);
+
     return (
         <>
-            <div className='todo-timer'>0:00</div>
+            <div className='todo-timer'>
+                <div className='time'>
+                    {seconds}s
+                </div>
+                <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={event => toggle(event)}>
+                    {isActive ? 'Pause' : 'Start'}
+                </button>
+            </div>
         </>
     )
 }
