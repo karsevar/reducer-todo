8677aa8a552dc90a1e18aab56e7160abcfc717ad
masonkarsevar <masonkarsevar@gmail.com>
2019-08-13 08:26:00 -0700

    created Form and Card components

---
+++ reducer-todo-project/src/components/TodoCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoCard(props) {
+    
+    return (
+        <div className='card-container'>
+
+        </div>
+    )
+}
+
+export default TodoCard;
---
+++ reducer-todo-project/src/components/TodoForm.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+function TodoForm (props) {
+
+    const [todo, setTodo] = useState('');
+
+    const handleChange = event => {
+        setTodo(event.target.value);
+    };
+
+    return (
+        <div className='form-container'>
+            <form>
+                <input 
+                    placeholder='todo'
+                    value={todo} 
+                    name='todo'
+                    onChange={handleChange}
+                />
+                <button>Submit!</button>
+            </form>
+        </div>
+    )
+}
+
+export default TodoForm;
