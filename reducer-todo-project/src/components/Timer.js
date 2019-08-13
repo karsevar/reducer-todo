40f10e1bd134e02d5bef182c41f79c4a2ac5764b
masonkarsevar <masonkarsevar@gmail.com>
2019-08-13 12:54:53 -0700

    timer refactored

--- reducer-todo-project/src/components/Timer.js
+++ reducer-todo-project/src/components/Timer.js
@@ -1,40 +1,34 @@
 import React, {useState, useEffect} from 'react';
 
 function Timer(props) {
-    const [seconds, setSeconds] = useState(0);
-    const [isActive, setIsActive] = useState(false);
-
-    function toggle(event) {
-        event.stopPropagation();
-        setIsActive(!isActive);
-    }
+    const [seconds, setSeconds] = useState(58);
+    const [minutes, setMinutes] = useState(0);
 
     function reset() {
         setSeconds(0);
-        setIsActive(false);
     }
+
     useEffect(() => {
         let interval = null;
-        if(isActive) {
-            interval = setInterval(() => {
-                setSeconds(seconds => seconds + 1);
-            }, 1000);
-        } else if (!isActive && seconds !== 0) {
-            clearInterval(interval);
+
+        interval = setInterval(() => {
+            setSeconds(seconds => seconds + 1);
+        }, 1000);
+
+        if (seconds === 60) {
+            reset();
+            setMinutes(minutes => minutes + 1) 
         }
 
         return () => clearInterval(interval);
-    }, [isActive, seconds]);
+    }, [seconds]);
 
     return (
         <>
             <div className='todo-timer'>
                 <div className='time'>
-                    {seconds}s
+                    {minutes} : {seconds}
                 </div>
-                <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={event => toggle(event)}>
-                    {isActive ? 'Pause' : 'Start'}
-                </button>
             </div>
         </>
     )
